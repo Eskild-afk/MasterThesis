dfb477d0e0ad3d99ee4f67c3df9739120a8992ec
VNemirov <victor@nemiroff.dk>
2023-04-12 09:26:02 +0200

    IM exploration

--- exposureGraphs.py
+++ exposureGraphs.py
@@ -5,7 +5,7 @@ from joblib import Parallel, delayed, cpu_count, dump, load
 from scipy.optimize import fsolve
 from HullWhite import HullWhite
 import time as timer
-
+from helpers import *
 import matplotlib.font_manager
 matplotlib.font_manager.findSystemFonts(fontpaths='C:\Windows\Fonts', fontext='ttf')
 
@@ -25,14 +25,7 @@ S=np.arange(0,11,1)
 
 # Other settings
 dt = 1/365
-sims = 5000
-
-#Helper functions 
-def pos (x):
-    return np.maximum(x,0)
-
-def neg (x):
-    return np.minimum(x,0)
+sims = 100
 
 #10Y Payer Swap Exposure
 if False:
@@ -148,7 +141,7 @@ if False:
     print( 'Finished creating graph')
 
 #5Y10Y Payer Swaption Exposure
-if True:
+if False:
     print('5Y10Y Payer Swaption Exposure')
     start = timer.time()
     time, float = HW.create_path(dt,15, seed=0)
@@ -207,7 +200,7 @@ if True:
     print( 'Finished creating graph')
 
 # Swap with Variation Margin
-if True:
+if False:
     print('10Y Payer Swap with Variation Margin')
     start = timer.time()
     time, float = HW.create_path(dt,10, seed=0)
@@ -287,7 +280,7 @@ if True:
 
 
     # Swaption with Variation Margin
-if True:
+if False:
     print('5Y10Y Payer Swaption Exposure with Variation Margin')
     start = timer.time()
     #5Y10Y Payer Swaption Exposure
@@ -365,4 +358,100 @@ if True:
     dump(PE/sims, f'./SimulationData/PE_5Y10Y_Swaption_With_VM_N={sims}_dt={int(1/dt)}.joblib')
     dump(NE/sims, f'./SimulationData/NE_5Y10Y_Swaption_With_VM_N={sims}_dt={int(1/dt)}.joblib')
     plt.savefig(f'./Graphs/Exposure_Plot_5Y10YPayer_Swaption_With_VM_N={sims}_dt={int(1/dt)}.png', bbox_inches='tight')
+    print( 'Finished creating graph')
+
+
+if True:
+    print('10Y Payer Swap with Variation and Initial Margin')
+    start = timer.time()
+    time, float = HW.create_path(dt,10, seed=0)
+    K=fsolve(lambda x: HW.swap(0, S, T, x), x0=0.02)[0]
+    KVM=0 # Threshold for VM
+    MTA=0 # Minimum Transfer Amount
+    lag=2 # Lookback lag
+    oneBP=0.0001
+    RW_6M=53
+    T_b=230
+    def worker(i):
+        time, float = HW.create_path(dt,10, seed=i)
+        swap = np.array([HW.swapextended(x[0], S, T, K=K, floatRate=float, schedule=time, initRate=x[1]) for x in np.array([time,float]).T])
+        
+        VM = []
+
+        for t in range(len(time)):
+            ttilde = t-lag
+            if (t == 0) or (ttilde <= 0):
+                VM.append(0)
+                continue
+            if t == len(time)-1:
+                VM.append(0)
+                break
+
+            VMhat = VM[ttilde-1]/HW.ZCB(time[ttilde-1], time[ttilde], initRate=float[ttilde-1]) # VM hat in eq 3.2 everything...
+            VMta  = VMhat #(ta: to append) 
+            
+            VMta += (abs(pos(swap[ttilde]-KVM)-pos(VMhat))>MTA)*(pos(swap[ttilde]-KVM)-pos(VMhat))
+            VMta += (abs(neg(swap[ttilde]+KVM)-neg(VMhat))>MTA)*(neg(swap[ttilde]+KVM)-neg(VMhat))
+
+            VM.append(VMta)
+            print('{:.2f}%'.format(round(i/sims*100, 2)), end='\r')
+        
+        swapBP = np.array([HW.swapextended(x[0], S, T, K=K, floatRate=float+oneBP, schedule=time, initRate=x[1]+oneBP) for x in np.array([time,float]).T])
+        IM = []
+        for t in range(len(time)):
+                ttilde = t-lag
+                if (t == 0) or (ttilde <= 0):
+                        IM.append(0)
+                        continue
+                if t == len(time)-1:
+                        IM.append(0)
+                        break
+                s = swapBP[ttilde]-swap[ttilde]
+                CR_B = np.maximum(1,np.sqrt(abs(s)/T_b))
+                IM.append(CR_B*s*RW_6M)
+
+        return np.maximum(swap-VM-IM,0), np.minimum(swap-VM+IM,0)
+        
+        
+
+    print('Starting parallel processing with {} cores'.format(cpu_count()))
+    results = Parallel(n_jobs=np.minimum(cpu_count(),sims))(delayed(worker)(i) for i in range(sims))
+    print('Finished parallel processing')
+    PE = np.zeros(len(time))
+    NE = np.zeros(len(time))
+    print('Starting sequential processing')
+    for i in range(len(results)):
+        PE += results[i][0]
+        NE += results[i][1]
+    print(f'Finished sequential processing in {timer.time()-start:.2f} seconds')
+    print('Creating graph')
+    discounting = np.array([HW.marketZCB(t) for t in time])
+
+    plt.rc('font',family='Times New Roman')
+    float_x = np.arange(0.5,10.5,0.5)
+    float_y = np.full((len(float_x),), -5.6)
+    fix_x = np.arange(1,11)
+    fix_y = np.full(len(fix_x), -5)
+
+    fig, ax = plt.subplots()
+    sns.lineplot(x=time, y=discounting*PE/sims*100, label = 'EPE')
+    sns.lineplot(x=time, y=discounting*NE/sims*100, label = 'ENE')
+    # plt.scatter(x=float_x, y=float_y, label = 'Float', marker = 'x', s = 60, c='black', linewidths=2)
+    # plt.scatter(x=fix_x, y=fix_y, label = 'Fix', marker = 'o', s = 60, facecolors='none', edgecolors='r', linewidths=2)
+    fig.set_size_inches(15,8)
+    # ax.set_ylim(-6,6)
+    # ax.set_xlim(0,10.1)
+    ax.set_xlabel('Time (Years)', fontname="Times New Roman", fontsize = 28)
+    ax.set_ylabel('Exposure (% Notional)', fontname="Times New Roman", fontsize = 28)
+    ax.tick_params(axis='x', direction='in', right = 'True', labelsize = 24, pad = 15)
+    ax.tick_params(axis='y', direction='in', top = 'True', labelsize = 24, pad = 15)
+    ax.xaxis.set_label_coords(0.5, -0.1)
+    ax.yaxis.set_label_coords(-0.08, 0.5)
+    ax.axhline(y=0, color='k', alpha = 0.25)
+    plt.grid(alpha = 0.25)
+    plt.xticks(np.arange(0, 10.5, 0.5))
+    plt.legend(frameon = False, fontsize = 18, loc='upper right')
+    dump(PE/sims, f'./SimulationData/PE_10Y_Swap_With_VM_AND_IM_N={sims}_dt={int(1/dt)}.joblib')
+    dump(NE/sims, f'./SimulationData/NE_10Y_Swap_With_VM_AND_IM_N={sims}_dt={int(1/dt)}.joblib')
+    plt.savefig(f'./Graphs/Exposure_Plot_10YPayer_Swap_With_VM_AND_IM_N={sims}_dt={int(1/dt)}.png', bbox_inches='tight')
     print( 'Finished creating graph')
--- helpers.py
+++ helpers.py
@@ -4,3 +4,9 @@ def find_nearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return array[idx]
+
+def pos (x):
+    return np.maximum(x,0)
+
+def neg (x):
+    return np.minimum(x,0)
